fb6c953c5d1575fc7bf23d5841628aaaa9676434
Kim JinSIk <99391603+Sweettreee@users.noreply.github.com>
2025-09-30 15:30:49 +0900

    Create assignment2.c

---
+++ assignment2.c
@@ -0,0 +1,22 @@
+// 과제
+#include <stdio.h>
+
+void PrintArray(빈 칸)
+{   
+    arr 전체 출력하세요
+}
+
+int main(void)
+{
+    int n = 10;
+    int arr[10][10];
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++){
+            arr[i][j] = i + j;
+        }
+
+    PrintArray(빈 칸);
+
+    return 0;
+}
